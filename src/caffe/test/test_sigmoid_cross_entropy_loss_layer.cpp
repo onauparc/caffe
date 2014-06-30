abb45264c241ea58c1374270049e47b2bd9de78d
Sergio <sguada@gmail.com>
2014-06-30 16:47:58 -0700

    Adapt test_sigmoid_sross_entropy to handle -1,0,1 labels

--- src/caffe/test/test_sigmoid_cross_entropy_loss_layer.cpp
+++ src/caffe/test/test_sigmoid_cross_entropy_loss_layer.cpp
@@ -18,10 +18,8 @@ namespace caffe {
 
 extern cudaDeviceProp CAFFE_TEST_CUDA_PROP;
 
-template <typename TypeParam>
-class SigmoidCrossEntropyLossLayerTest : public MultiDeviceTest<TypeParam> {
-  typedef typename TypeParam::Dtype Dtype;
-
+template <typename Dtype>
+class SigmoidCrossEntropyLossLayerTest : public ::testing::Test {
  protected:
   SigmoidCrossEntropyLossLayerTest()
       : blob_bottom_data_(new Blob<Dtype>(10, 5, 1, 1)),
@@ -34,10 +32,13 @@ class SigmoidCrossEntropyLossLayerTest : public MultiDeviceTest<TypeParam> {
     blob_bottom_vec_.push_back(blob_bottom_data_);
     // Fill the targets vector
     FillerParameter targets_filler_param;
-    targets_filler_param.set_min(0);
+    targets_filler_param.set_min(-1);
     targets_filler_param.set_max(1);
     UniformFiller<Dtype> targets_filler(targets_filler_param);
     targets_filler.Fill(blob_bottom_targets_);
+    int count = blob_bottom_targets_->count();
+    caffe_cpu_sign(count, this->blob_bottom_targets_->cpu_data(),
+      this->blob_bottom_targets_->mutable_cpu_data());
     blob_bottom_vec_.push_back(blob_bottom_targets_);
   }
   virtual ~SigmoidCrossEntropyLossLayerTest() {
@@ -54,9 +55,11 @@ class SigmoidCrossEntropyLossLayerTest : public MultiDeviceTest<TypeParam> {
       EXPECT_LE(prediction, 1);
       EXPECT_GE(prediction, 0);
       EXPECT_LE(target[i], 1);
-      EXPECT_GE(target[i], 0);
-      loss -= target[i] * log(prediction + (target[i] == Dtype(0)));
-      loss -= (1 - target[i]) * log(1 - prediction + (target[i] == Dtype(1)));
+      EXPECT_GE(target[i], -1);
+      if (target[i] != 0) {
+        loss -= (target[i] > 0) * log(prediction + (target[i] < 0));
+        loss -= (target[i] < 0) * log(1 - prediction + (target[i] > 0));
+      }
     }
     return loss / num;
   }
@@ -67,20 +70,23 @@ class SigmoidCrossEntropyLossLayerTest : public MultiDeviceTest<TypeParam> {
     data_filler_param.set_std(1);
     GaussianFiller<Dtype> data_filler(data_filler_param);
     FillerParameter targets_filler_param;
-    targets_filler_param.set_min(0.0);
-    targets_filler_param.set_max(1.0);
+    targets_filler_param.set_min(-1);
+    targets_filler_param.set_max(1);
     UniformFiller<Dtype> targets_filler(targets_filler_param);
+    const int count = this->blob_bottom_data_->count();
     Dtype eps = 2e-2;
-    for (int i = 0; i < 100; ++i) {
+    for (int i = 0; i < 10; ++i) {
       // Fill the data vector
       data_filler.Fill(this->blob_bottom_data_);
       // Fill the targets vector
       targets_filler.Fill(this->blob_bottom_targets_);
+      // Make negatives into -1 and positives into 1
+      Dtype* targets = this->blob_bottom_targets_->mutable_cpu_data();
+      caffe_cpu_sign(count, targets, targets);
       SigmoidCrossEntropyLossLayer<Dtype> layer(layer_param);
       layer.SetUp(this->blob_bottom_vec_, &(this->blob_top_vec_));
       Dtype layer_loss =
           layer.Forward(this->blob_bottom_vec_, &(this->blob_top_vec_));
-      const int count = this->blob_bottom_data_->count();
       const int num = this->blob_bottom_data_->num();
       const Dtype* blob_bottom_data = this->blob_bottom_data_->cpu_data();
       const Dtype* blob_bottom_targets =
@@ -97,18 +103,67 @@ class SigmoidCrossEntropyLossLayerTest : public MultiDeviceTest<TypeParam> {
   vector<Blob<Dtype>*> blob_top_vec_;
 };
 
-TYPED_TEST_CASE(SigmoidCrossEntropyLossLayerTest, TestDtypesAndDevices);
+typedef ::testing::Types<float, double> Dtypes;
+TYPED_TEST_CASE(SigmoidCrossEntropyLossLayerTest, Dtypes);
 
-TYPED_TEST(SigmoidCrossEntropyLossLayerTest, TestSigmoidCrossEntropyLoss) {
+TYPED_TEST(SigmoidCrossEntropyLossLayerTest, TestSetup1Top) {
+  LayerParameter layer_param;
+  SigmoidCrossEntropyLossLayer<TypeParam> layer(layer_param);
+  vector<Blob<TypeParam>*> aux_top_vec;
+  Blob<TypeParam>* blob_top_ = new Blob<TypeParam>();
+  aux_top_vec.push_back(blob_top_);
+  layer.SetUp(this->blob_bottom_vec_, &(aux_top_vec));
+  EXPECT_EQ(blob_top_->num(), 1);
+  EXPECT_EQ(blob_top_->channels(), 1);
+  EXPECT_EQ(blob_top_->height(), 1);
+  EXPECT_EQ(blob_top_->width(), 1);
+}
+
+TYPED_TEST(SigmoidCrossEntropyLossLayerTest, TestSetup2Tops) {
+  LayerParameter layer_param;
+  SigmoidCrossEntropyLossLayer<TypeParam> layer(layer_param);
+  vector<Blob<TypeParam>*> aux_top_vec;
+  Blob<TypeParam>* blob_top_ = new Blob<TypeParam>();
+  Blob<TypeParam>* blob_top2_ = new Blob<TypeParam>();
+  aux_top_vec.push_back(blob_top_);
+  aux_top_vec.push_back(blob_top2_);
+  layer.SetUp(this->blob_bottom_vec_, &(aux_top_vec));
+  EXPECT_EQ(blob_top_->num(), 1);
+  EXPECT_EQ(blob_top_->channels(), 1);
+  EXPECT_EQ(blob_top_->height(), 1);
+  EXPECT_EQ(blob_top_->width(), 1);
+  EXPECT_EQ(blob_top2_->num(), this->blob_bottom_targets_->num());
+  EXPECT_EQ(blob_top2_->channels(), this->blob_bottom_targets_->channels());
+  EXPECT_EQ(blob_top2_->height(), this->blob_bottom_targets_->height());
+  EXPECT_EQ(blob_top2_->width(), this->blob_bottom_targets_->width());
+}
+
+TYPED_TEST(SigmoidCrossEntropyLossLayerTest, TestSigmoidCrossEntropyLossCPU) {
+  Caffe::set_mode(Caffe::CPU);
   this->TestForward();
 }
 
-TYPED_TEST(SigmoidCrossEntropyLossLayerTest, TestGradient) {
-  typedef typename TypeParam::Dtype Dtype;
+TYPED_TEST(SigmoidCrossEntropyLossLayerTest, TestSigmoidCrossEntropyLossGPU) {
+  Caffe::set_mode(Caffe::GPU);
+  this->TestForward();
+}
+
+TYPED_TEST(SigmoidCrossEntropyLossLayerTest, TestGradientCPU) {
   LayerParameter layer_param;
-  SigmoidCrossEntropyLossLayer<Dtype> layer(layer_param);
+  Caffe::set_mode(Caffe::CPU);
+  SigmoidCrossEntropyLossLayer<TypeParam> layer(layer_param);
   layer.SetUp(this->blob_bottom_vec_, &this->blob_top_vec_);
-  GradientChecker<Dtype> checker(1e-2, 1e-2, 1701);
+  GradientChecker<TypeParam> checker(1e-2, 1e-2, 1701);
+  checker.CheckGradientSingle(&layer, &(this->blob_bottom_vec_),
+      &(this->blob_top_vec_), 0, -1, -1);
+}
+
+TYPED_TEST(SigmoidCrossEntropyLossLayerTest, TestGradientGPU) {
+  LayerParameter layer_param;
+  Caffe::set_mode(Caffe::GPU);
+  SigmoidCrossEntropyLossLayer<TypeParam> layer(layer_param);
+  layer.SetUp(this->blob_bottom_vec_, &this->blob_top_vec_);
+  GradientChecker<TypeParam> checker(1e-2, 1e-2, 1701);
   checker.CheckGradientSingle(&layer, &(this->blob_bottom_vec_),
       &(this->blob_top_vec_), 0, -1, -1);
 }
