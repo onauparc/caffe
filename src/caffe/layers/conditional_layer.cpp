4b5679763d7c107a59d212fd09a01f170ad34c6e
manuele <manuele.tamburrano@gmail.com>
2014-11-11 19:23:56 +0100

    implementato conditional layer

--- src/caffe/layers/conditional_layer.cpp
+++ src/caffe/layers/conditional_layer.cpp
@@ -9,104 +9,69 @@ namespace caffe {
 template <typename Dtype>
 void ConditionalLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
-  concat_dim_ = this->layer_param_.concat_param().concat_dim();
-  CHECK_GE(concat_dim_, 0) <<
-    "concat_dim should be >= 0";
-  CHECK_LE(concat_dim_, 1) <<
-    "For now concat_dim <=1, it can only concat num and channels";
+  conditional_index_ = this->layer_param_.conditional_param().conditional_index();
+  CHECK_LE(conditional_index_,bottom[0]->channels()-1 ) <<
+    "conditional_index_ should be <= bottom[0]->channels()-1";
 }
 
 template <typename Dtype>
 void ConditionalLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
-  // Initialize with the first blob.
-  count_ = bottom[0]->count();
-  num_ = bottom[0]->num();
-  channels_ = bottom[0]->channels();
-  height_ = bottom[0]->height();
-  width_ = bottom[0]->width();
-  for (int i = 1; i < bottom.size(); ++i) {
-    count_ += bottom[i]->count();
-    if (concat_dim_== 0) {
-      num_ += bottom[i]->num();
-    } else if (concat_dim_ == 1) {
-      channels_ += bottom[i]->channels();
-    } else if (concat_dim_ == 2) {
-      height_ += bottom[i]->height();
-    } else if (concat_dim_ == 3) {
-      width_ += bottom[i]->width();
-    }
-  }
-  top[0]->Reshape(num_, channels_, height_, width_);
-  CHECK_EQ(count_, top[0]->count());
-}
-
-void softmax_casareccio(std::vector<float> y, std::vector<float>& value)
-{
-  //Vector y = mlp(x); // output of the neural network without softmax activation function
-  double ysum = 0;
-  for(int f = 0; f < y.size(); f++)
-    ysum += exp((double)y[f]);
+  //bottom[0] is the "IF_blob"
+  //bottom[1] is the "THEN_blob"
+  //top[0] is the vector of indices that passed the conditional check
+  //top[1] is the top to which will be forwarded bottom[1]
+  top[0]->Reshape(1, 1, 1, 1);
+  top[1]->Reshape(bottom[1]->num(), bottom[1]->channels(), bottom[1]->height(), bottom[1]->width());
   
-  for(int i = 0; i<value.size(); i++)
-  {
-    value[i] = (float)(exp((double)value[i])/ysum);
-  }
 }
 
 template <typename Dtype>
 void ConditionalLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
-        
-        
-        
-  //const float* bottom_data_pettorina = bottom[0]->cpu_data();
-  //const float* bottom_data_pool1 = bottom[1]->cpu_data();
-  const Dtype* bottom_data_pettorina = bottom[0]->cpu_data();
-  const Dtype* bottom_data_pool1 = bottom[1]->cpu_data();
-  //const float* bottom_label = result[1]->cpu_data();
-  vector<float> indicesTokeep;
-  int max = -1;
-  float max_prob = -FLT_MAX;
-
-  int num_labels_pettorine = 2;
-  int num_images = bottom[0]->num();
-  LOG(ERROR) << "num_images: "<<num_images;
-  int ind_pettorine = 0;
-  for (int z = 0; z < num_images; ++z) {
+  
+  const Dtype* bottom_data_IF = bottom[0]->cpu_data();
+  const Dtype* bottom_data_THEN = bottom[1]->cpu_data();
+  
+  vector<float> indices_to_keep;
+  int num_items = bottom[0]->num();
+  int num_channels = bottom[0]->channels();
+  //look through the batch to find who passes the conditional check
+  for (size_t item_id = 0; item_id < num_items; ++item_id) {
     
-    ind_pettorine = num_labels_pettorine*z;
+    int index_IF = item_id*num_channels;
+    const Dtype* tmp_data_IF = bottom_data_IF + index_IF;
+    int argmax = std::distance(tmp_data_IF,
+        std::max_element(tmp_data_IF, tmp_data_IF + num_channels));
     
-    vector<float> is_pett;
-    
-    is_pett.push_back((float)bottom_data_pettorina[ind_pettorine]);
-    is_pett.push_back((float)bottom_data_pettorina[ind_pettorine + 1]);
-    LOG(ERROR) <<" bottom_data_pettorina[ind_pettorine]: "<<(float)bottom_data_pettorina[ind_pettorine];
-    LOG(ERROR) <<" bottom_data_pettorina[ind_pettorine+1]: "<<(float)bottom_data_pettorina[ind_pettorine+1];
-    
-    softmax_casareccio(is_pett, is_pett);
-    float max_isPett = *(std::max_element(is_pett.begin(),is_pett.end()));
-    float max_isPett_index = distance(is_pett.begin(), max_element(is_pett.begin(), is_pett.end()));
-    
-    stringstream ss;
-    float THRESH_PETT = 0.5;
-     if(max_isPett >= THRESH_PETT && max_isPett_index == 1)
-     {
-      indicesTokeep.push_back(z);
-      LOG(ERROR) << z<<") PRESO - max_isPett_index: "<<max_isPett_index<<"(PETTORINA) - value: "<<max_isPett;
-     }
-     else
-      LOG(ERROR) << z<<") SCARTATO - max_isPett_index: "<<max_isPett_index<<"(NON PETTORINA) - value: "<<max_isPett;
-   
+    if (argmax == conditional_index_)
+      indices_to_keep.push_back(item_id);
+      
   }
   
-  top[0]->Reshape(indicesTokeep.size(), top[0]->channels(), top[0]->height(), top[0]->width());     
-  Dtype* top_data = top[0]->mutable_cpu_data();
-  int index = 0;
-  for(size_t n = 0; n<indicesTokeep.size(); n++, index++)
+  //Only items that passed conditional check will be forwarded
+  int new_tops_num = indices_to_keep.size();
+  if (new_tops_num == 0)
+    new_tops_num = 1;
+
+  top[0]->Reshape(new_tops_num, 1, 1, 1);
+  top[1]->Reshape(new_tops_num, top[1]->channels(), top[1]->height(), top[1]->width());
+
+  Dtype* top_data_indices = top[0]->mutable_cpu_data();
+  Dtype* top_data = top[1]->mutable_cpu_data();
+  
+  size_t size_top = top[1]->count()/top[1]->num();
+  for (size_t n = 0; n<indices_to_keep.size(); n++)
   {
-    int offset = indicesTokeep[n];
-    top_data[index] = bottom_data_pool1[offset];
+    int offset = indices_to_keep[n];
+    top_data_indices[n] = (float)offset;
+    int data_offset_top = size_top*n;
+    int data_offset_bottom = size_top*offset;
+    for(size_t s = 0; s<size_top; s++)
+    {
+      top_data[data_offset_top + s] = bottom_data_THEN[data_offset_bottom + s];
+    }
+
   }
 
 }
